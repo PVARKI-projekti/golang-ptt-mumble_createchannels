f26475fc4f2baef5d429a1dda497b8b6b4ef4695
Eero af Heurlin <eero.afheurlin@iki.fi>
2022-09-30 17:24:42 +0300

    bump version

--- mumble_createchannels.go
+++ mumble_createchannels.go
@@ -13,7 +13,7 @@ import (
 var Rev = ""
 
 // Version is incremented using bump2version
-const Version = "0.0.1"
+const Version = "0.0.2"
 
 // Shout returns the input message with an exclamation mark
 func Shout(s string) string {
