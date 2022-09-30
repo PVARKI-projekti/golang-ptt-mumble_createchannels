95c82d37f7630429ee097fa472939015d8c38b0f
Eero af Heurlin <eero.afheurlin@iki.fi>
2022-10-01 00:18:03 +0300

    connecting to a server works

--- mumble_createchannels.go
+++ mumble_createchannels.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"crypto/tls"
+	"net"
 	"os"
+	"strconv"
 
 	"github.com/romnn/flags4urfavecli/flags"
-	"github.com/romnn/flags4urfavecli/values"
 	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
+	"layeh.com/gumble/gumble"
 )
 
 // Rev is set on build time to the git HEAD
@@ -15,32 +18,94 @@ var Rev = ""
 // Version is incremented using bump2version
 const Version = "0.0.2"
 
-// Shout returns the input message with an exclamation mark
-func Shout(s string) string {
-	return s + "!"
-}
-
 func main() {
 	app := &cli.App{
-		Name:  "mumble_createchannels",
-		Usage: "",
+		Version:   Version,
+		Usage:     "Create channel hierarchies on mumble server",
+		Name:      "mumble_createchannels",
+		ArgsUsage: "server_address",
 		Flags: []cli.Flag{
-			&cli.GenericFlag{
-				Name: "format",
-				Value: &values.EnumValue{
-					Enum:    []string{"json", "xml", "csv"},
-					Default: "xml",
-				},
-				EnvVars: []string{"FILEFORMAT"},
-				Usage:   "input file format",
-			},
 			&flags.LogLevelFlag,
+			&cli.PathFlag{
+				Name:  "cert",
+				Usage: "Path to certificate `FILE` (PEM)",
+				//Required: true,
+			},
+			&cli.PathFlag{
+				Name:  "key",
+				Usage: "Path to key `FILE` (PEM)",
+				//Required: true,
+			},
+			&cli.StringFlag{
+				Name:  "pass",
+				Usage: "Server password",
+			},
+			&cli.IntFlag{
+				Name:  "port",
+				Usage: "Server `PORT`",
+				Value: gumble.DefaultPort,
+			},
+			&cli.StringFlag{
+				Name:  "user",
+				Usage: "Username to use",
+				Value: "gumble_channel_bot",
+			},
+			&cli.BoolFlag{
+				Name:  "insecure",
+				Usage: "Do not verify server certificate",
+				Value: false,
+			},
 		},
 		Action: func(ctx *cli.Context) error {
 			if level, err := log.ParseLevel(ctx.String("log")); err == nil {
 				log.SetLevel(level)
 			}
-			log.Infof("Format is: %s", ctx.String("format"))
+			if ctx.Args().Len() < 1 {
+				log.Error("server address missing")
+				cli.ShowAppHelpAndExit(ctx, 1)
+			}
+
+			var tlsConfig tls.Config
+			if ctx.Bool("insecure") {
+				tlsConfig.InsecureSkipVerify = true
+			}
+			cert := ctx.Path("cert")
+			if cert != "" {
+				key := ctx.Path("key")
+				if key == "" {
+					// In case cert and key are in same file
+					key = cert
+				}
+				certificate, err := tls.LoadX509KeyPair(cert, key)
+				if err != nil {
+					log.Error(err)
+					return cli.Exit("Cannot load certificate", 1)
+				}
+				tlsConfig.Certificates = append(tlsConfig.Certificates, certificate)
+			}
+
+			config := gumble.NewConfig()
+			config.Username = ctx.String("user")
+			serverpass := ctx.String("pass")
+			if serverpass != "" {
+				config.Password = serverpass //pragma: allowlist secret
+			}
+
+			address := net.JoinHostPort(ctx.Args().First(), strconv.Itoa(ctx.Int("port")))
+
+			log.Info("Dialing ", address)
+			client, err := gumble.DialWithDialer(new(net.Dialer), address, config, &tlsConfig)
+			if err != nil {
+				log.Error(err)
+				return cli.Exit("Could not connect", 1)
+			}
+
+			log.Info("Disconnecting")
+			err = client.Disconnect()
+			if err != nil {
+				log.Error(err)
+				return cli.Exit("Could not disconnect", 1)
+			}
 			return nil
 		},
 	}
